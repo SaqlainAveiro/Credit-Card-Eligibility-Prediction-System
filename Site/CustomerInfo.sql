b1091d128cf047e23cddbf46158cb25a64357356
Md. Anwarul Habib <saqlainahs@gmail.com>
2022-03-17 15:57:45 +0600

    Add files via upload

---
+++ Site/CustomerInfo.sql
@@ -0,0 +1,11 @@
+--Drop Table CustomerInfo;
+
+Create Table CustomerInfo(
+    Customer_ID NUMBER,
+	Customer_Name VARCHAR2(20),
+	Customer_Age NUMBER,
+	Customer_CreditCardOffer NUMBER
+);
+
+
+Commit;
